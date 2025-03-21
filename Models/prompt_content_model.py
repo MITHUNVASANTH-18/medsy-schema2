27cea18ad11029db1c0aea35ad2f60cd12f8e6f7
naveenkumar <naveenkumar@wizzgeeks.com>
2025-03-21 14:46:20 +0530

    added prompt changes

--- Models/prompt_content_model.py
+++ Models/prompt_content_model.py
@@ -1,12 +1,13 @@
 from mongoengine import Document,ReferenceField,DictField,ListField,StringField
 from Models.course_model import Course
-
+from Models.model_model import Model
 
 class Prompt_content(Document):
     name = StringField(required=True)
     course = ReferenceField(Course,reverse_delete_rule=2,required=True)
     system_content = DictField(required=True)
     framework = ListField(DictField())
+    model = ReferenceField(Model,reverse_delete_rule=2,required=True)
 
   
     def to_json(self):
@@ -16,4 +17,5 @@ class Prompt_content(Document):
             'course':str(self.course.id),
             'system_content':str(self.system_content),
             'framework':str(self.framework),
+            'model':str(self.model.id)
         }
