25d0487488fa1d82b025cb566d9663b71706e2be
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2025-03-21 15:38:27 +0530

    job schema created

---
+++ Models/job_detail.py
@@ -0,0 +1,28 @@
+from mongoengine import Document,StringField,IntField,ReferenceField,DateTimeField,ListField,DictField
+from Models.admin_model import Admin
+from Models.course_model import Course
+from datetime import datetime
+
+
+class Job_detail(Document):
+    course = ReferenceField(Course,reverse_delete_rule=2,required=True)
+    created_by=ReferenceField(Admin,required=True,reverse_delete_rule=2)
+    created_at = DateTimeField(default=datetime.utcnow)
+    target=StringField()
+    detail=StringField()
+    completed_count=IntField()
+    total_count=IntField()
+    status=StringField()
+
+    def to_json(self):
+        return{
+            "course":str(self.course.id) if self.course else None
+            ,"created_by":str(self.created_by.id) if self.created_by else None,
+            "created_at":str(self.created_at),
+            "target":self.target,
+            "detail":self.detail,
+            "completed_count":self.completed_count,
+            "total_count":self.total_count,
+            "status":self.status
+
+        }
